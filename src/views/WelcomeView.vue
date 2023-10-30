055038e1f680a4ebeb44252b284dc4f32dce0557
MarinaGalzerano <marinagalzerano98@gmail.com>
2023-10-30 19:52:25 -0300

    Modificación imagen en Home

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="centered-container">
-    <img class="logo" src="../assets/logo_gran_dt_ort.png">
+    <img class="logo" src="../assets/logo_gran_dt_ort.png" />
     <div class="centered-container box">
       <h1 class="titulo">Bienvenido a El Gran DT ORT</h1>
       <button class="btn btn-primary" @click="goToLogin">Iniciar Sesión</button>
+      <td></td>
       <button class="btn btn-primary" @click="goToRegister">Registrarse</button>
     </div>
   </div>
@@ -13,14 +14,14 @@
 export default {
   methods: {
     goToRegister() {
-      this.$router.push('/register');
+      this.$router.push("/register");
     },
     goToLogin() {
-      this.$router.push('/login');
-    }
-  }
+      this.$router.push("/login");
+    },
+  },
 };
 </script>
 <style scoped>
-@import url("C:\Users\Usuario\OneDrive\Escritorio\JUAN_PROGRAMACION\PNT2\ElGranDT_ORT\src\assets\estilos.css");
+@import url("C:\Users\Usuario\OneDrive\Escritorio\PNT2-2023\el_gran_dt_ort\src\assets\estilos.css");
 </style>
