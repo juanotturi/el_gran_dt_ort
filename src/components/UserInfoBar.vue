b515b4c6aa9a7f30e7059bccafef9e13872c986d
juanotturi <juanotturi@gmail.com>
2023-11-03 19:05:15 -0300

    logout hecho y left bar en desarrollo

---
+++ src/components/UserInfoBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="left-panel">
+        <p>Equipo:</p>
+        <p></p>
+        <p>DT:</p>
+        <p>{{ user }}</p>
+        <p>Presupuesto total:</p>
+        <p></p>
+        <p>Utilizado:</p>
+        <p></p>
+        <button class="btn btn-danger" @click="logout">Cerrar Sesión</button>
+    </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+import { useUserStore } from '../stores/user.js';
+
+const router = useRouter();
+const userStore = useUserStore();
+const user = userStore.currentUser;
+
+function logout() {
+    userStore.logout();
+    router.push("/");
+}
+</script>
+
+<style>
+@import url("@/assets/estilos.css");
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="centered-container">
+    <UserInfoBar />
     <img class="cancha" src="../assets/cancha.png" />
   </div>
 </template>
+
+<script setup>
+import UserInfoBar from '../components/UserInfoBar.vue';
+</script>
 
 <style scoped>
 @import url("@/assets/estilos.css");
