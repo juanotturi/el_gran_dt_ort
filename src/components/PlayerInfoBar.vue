79aacdc6e2e72c142f8a154aaeb23ab7490d9884
juanotturi <juanotturi@gmail.com>
2023-11-04 23:07:08 -0300

    barra derecha completa

--- src/components/PlayerInfoBar.vue
+++ src/components/PlayerInfoBar.vue
@@ -1,18 +1,19 @@
 <template>
     <div class="right-panel">
         <div class="logo-container">
-            <img width="150" src="../assets/logo_gran_dt_ort.png" alt="logo" />
+            <img width="150" src="../assets/logo_gran_dt_ort.png" alt="logo"
+                style="margin-top: -18px; margin-bottom: -18px;" />
         </div>
         <div class="panel-item">
-            <p>Nombre:<br></p>
-        </div>
-        <div class="divider"></div>
-        <div class="panel-item">
-            <p>Club:<br></p>
+            <p>Nombre:<br>{{ selectedPlayer ? selectedPlayer.name : '' }}</p>
         </div>
         <div class="divider"></div>
         <div class="panel-item">
-            <p>Cotización:<br></p>
+            <p>Club:<br>{{ selectedPlayer ? selectedPlayer.club : '' }}</p>
+        </div>
+        <div class="divider"></div>
+        <div class="panel-item">
+            <p>Cotización:<br>{{ selectedPlayer ? selectedPlayer.market_value : '' }}</p>
         </div>
         <div class="divider"></div>
         <div>
@@ -36,18 +37,17 @@ import Button from 'primevue/button';
 
 const showList = ref(false);
 const selectedPlayer = ref(null);
-
 const players = [
-    { id: "001", name: 'Adam Bareriro', club: 'San Lorenzo', position: 'delantero', market_value: '150000' },
-    { id: "002", name: 'Luís Advíncula', club: 'Boca Juniors', position: 'defensor', market_value: '200000' },
-    { id: "003", name: 'Enzo Pérez', club: 'River Plate', position: 'volante', market_value: '160000' },
-    { id: "004", name: 'Augusto Batalla', club: 'San Lorenzo', position: 'arquero', market_value: '200000' },
-    { id: "005", name: 'Marcos Rojo', club: 'Boca Juniors', position: 'defensor', market_value: '160000' },
-    { id: "006", name: 'Matías Cóccaro', club: 'Huracán', position: 'delanter', market_value: '150000' },
-    { id: "007", name: 'Franco Armani', club: 'River Plate', position: 'arquero', market_value: '200000' },
-    { id: "008", name: 'Agustín Giay', club: 'San Lorenzo', position: 'volante', market_value: '180000' },
-    { id: "009", name: 'Sergio Romero', club: 'Boca Juniors', position: 'arquero', market_value: '150000' },
-    { id: "010", name: 'Salomón Rondón', club: 'River Plate', position: 'delantero', market_value: '250000' },
+    { id: "001", name: 'Bareriro Adam', club: 'San Lorenzo', position: 'delantero', market_value: 4200000 },
+    { id: "002", name: 'Advíncula Luís', club: 'Boca Juniors', position: 'defensor', market_value: 4700000 },
+    { id: "003", name: 'Pérez Enzo', club: 'River Plate', position: 'volante', market_value: 4200000 },
+    { id: "004", name: 'Batalla Augusto', club: 'San Lorenzo', position: 'arquero', market_value: 5000000 },
+    { id: "005", name: 'Rojo Marcos', club: 'Boca Juniors', position: 'defensor', market_value: 5000000 },
+    { id: "006", name: 'Cóccaro Matías', club: 'Huracán', position: 'delanter', market_value: 4500000 },
+    { id: "007", name: 'Armani Franco', club: 'River Plate', position: 'arquero', market_value: 5900000 },
+    { id: "008", name: 'Giay Agustín', club: 'San Lorenzo', position: 'volante', market_value: 1400000 },
+    { id: "009", name: 'Romero Sergio', club: 'Boca Juniors', position: 'arquero', market_value: 6000000 },
+    { id: "010", name: 'Rondón Salomón', club: 'River Plate', position: 'delantero', market_value: 5000000 },
 ];
 function openList() {
     showList.value = !showList.value;
