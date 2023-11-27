6d4bfc025ed43cbe9f147b6d0595528dd163d42d
juanotturi <juanotturi@gmail.com>
2023-11-27 00:00:19 -0300

    se restauro login viejo por errores graficos en field

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,45 +1,29 @@
 <template>
   <div class="centered-container">
-    <div class="logo-container">
-      <img class="logo" src="../assets/logo_gran_dt_ort.png" alt="logo" />
-    </div>
+    <img class="logo" src="../assets/logo_gran_dt_ort.png" alt="logo" />
     <div class="centered-container box">
-      <div class="login-box">
-        <form @submit.prevent="login">
-          <h2 style="color: rgb(249, 246, 246)">Iniciar Sesión</h2>
-          <br />
-          <div class="user-box">
-            <input type="text" v-model="mail" required />
-            <label>Usuario</label>
-          </div>
-          <div class="user-box">
-            <input type="password" v-model="password" required />
-            <label>Contraseña</label>
-          </div>
-          <center>
-            <button type="submit" style="display: none"></button>
-            <a href="#" @click="submitForm" class="custom-button">
-              Ingresar
-              <span></span>
-            </a>
-          </center>
-        </form>
+      <h1 class="titulo">Inicio de sesión</h1>
+      <div>
+        <input type="text" id="mail" v-model="mail" placeholder="Correo electrónico" required />
+      </div>
+      <div>
+        <input type="password" id="password" v-model="password" placeholder="Contraseña" required />
+      </div>
+      <div>
+        <Button @click="login" label="Ingresar" severity="info" />
       </div>
     </div>
   </div>
 </template>
-
 <script setup>
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { useUserStore } from '../stores/userStore.js';
 import Button from 'primevue/button';
-
 const mail = ref('');
 const password = ref('');
 const router = useRouter();
 const userStore = useUserStore();
-
 async function login() {
   const loginResult = await userStore.login(mail.value, password.value);
   if (loginResult) {
@@ -48,10 +32,6 @@ async function login() {
     alert('Usuario o contraseña incorrectos');
   }
 };
-function submitForm() {
-  const submitButton = document.querySelector("button[type=submit]");
-  submitButton.click();
-}
 </script>
 
 <style scoped>
