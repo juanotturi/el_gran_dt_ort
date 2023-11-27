18595880fa1685f9546a8edf5d303b92d4d90bec
juanotturi <juanotturi@gmail.com>
2023-11-27 12:52:25 -0300

    se mejoro el login con estilos de marina

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,19 +2,22 @@
   <div class="centered-container">
     <img class="logo" src="../assets/logo_gran_dt_ort.png" alt="logo" />
     <div class="centered-container box">
-      <h1 class="titulo">Inicio de sesión</h1>
-      <div>
-        <input type="text" id="mail" v-model="mail" placeholder="Correo electrónico" required />
-      </div>
-      <div>
-        <input type="password" id="password" v-model="password" placeholder="Contraseña" required />
-      </div>
-      <div>
-        <Button @click="login" label="Ingresar" severity="info" />
+      <div id="formularioLogin">
+        <h1 class="titulo">Iniciar sesión</h1>
+        <div>
+          <input type="text" id="mail" v-model="mail" placeholder="Correo electrónico" required />
+        </div>
+        <div>
+          <input type="password" id="password" v-model="password" placeholder="Contraseña" required />
+        </div>
+        <div>
+          <Button @click="login" label="Ingresar" severity="info" />
+        </div>
       </div>
     </div>
   </div>
 </template>
+
 <script setup>
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
